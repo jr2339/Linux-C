8b32721accdfd6f950601d4aacfd5ddc1d53cee9
jr2339 <jr2339@nau.edu>
2016-12-19 19:08:48 -0500

    .

---
+++ CLanguage/3-Array.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(int argc,char *argv[]){
+    
+    char message[12] = "Hello World"; // this is how we declare an array of char which is String
+    
+    printf("message is '%s'\n", message);
+    
+    return 0;
+}
